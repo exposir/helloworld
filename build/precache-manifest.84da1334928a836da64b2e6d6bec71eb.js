da2bb42692081b0f0568b22b0b18ac23ce570296
menglingyu <897205285@qq.com>
2020-03-20 00:06:14 +0800

    test

---
+++ build/precache-manifest.84da1334928a836da64b2e6d6bec71eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4525c7c13bdcda192eb349611e31fef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80004d94c22962593c0a",
+    "url": "/static/css/main.bf13b2b3.chunk.css"
+  },
+  {
+    "revision": "195c84c5c6f0ce8f0110",
+    "url": "/static/js/2.3c96e888.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.3c96e888.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80004d94c22962593c0a",
+    "url": "/static/js/main.314f0b26.chunk.js"
+  },
+  {
+    "revision": "ec3c970b14462cc81fcf",
+    "url": "/static/js/runtime-main.67960764.js"
+  }
+]);
